7601633cce797b5cfffc2c87d4e16d95ba40c23d
GreenVolcan0 <ba@aqa.ru>
2024-02-03 16:52:14 +0300

    Задание 1 закончено

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# Задание 1
 f = open('products.csv', encoding = "UTF8").read()
 f = f.split('\n')
 a = []
@@ -7,7 +9,11 @@ for el in a:
     el.append('0')
     if "0" in el[5]:
         el[5] = str(float(el[3])*float(el[4]))
-print(a)
+summa = 0
+for el in a:
+    if "Закуски" in el[0]:
+        summa += round(float((el[5])))
+print(summa)
 
 
 
